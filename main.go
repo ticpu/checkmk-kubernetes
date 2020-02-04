049ddf5343fd6d82a92874829c429b61fa68e1d7
Jérôme Poulin <jerome.poulin@cauca.ca>
2020-02-04 01:01:46 -0500

    Add capacity calculation.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"log"
@@ -56,7 +57,17 @@ func parseConfig () []rest.Config {
 	return kubeConfigList
 }
 
+type nodeResourceUsage struct {
+	maxMemoryAvailable resource.Quantity
+	memoryRequest resource.Quantity
+	memoryLimit resource.Quantity
+	maxPodsAvailable int64
+	pods int64
+}
+
 func main () {
+	var nodeResource nodeResourceUsage
+	nodeResources := make(map[string]nodeResourceUsage)
 	kubeConfigList := parseConfig()
 
 	for _, kubeConfig := range kubeConfigList {
@@ -79,10 +90,42 @@ func main () {
 			continue
 		}
 
-		fmt.Printf("There are %d pods in the cluster.\n", len(pods.Items))
+		fmt.Printf("There are %d nodes in the cluster.\n", len(nodes.Items))
 		for _, node := range nodes.Items {
-			fmt.Printf("Node: %s, %v\n", node.Name, node.Status.Allocatable)
+			nodeResources[node.Name] = nodeResourceUsage{
+				maxMemoryAvailable: node.Status.Allocatable.Memory().DeepCopy(),
+				maxPodsAvailable:   node.Status.Allocatable.Pods().Value(),
+				memoryRequest:      resource.Quantity{},
+				memoryLimit:        resource.Quantity{},
+				pods:               0,
+			}
 		}
+
+		fmt.Printf("There are %d pods in the cluster.\n", len(pods.Items))
+		for _, pod := range pods.Items {
+			nodeResource = nodeResources[pod.Spec.NodeName]
+			nodeResource.pods++
+			for _, container := range pod.Spec.Containers {
+				nodeResource.memoryLimit.Add(*container.Resources.Limits.Memory())
+				nodeResource.memoryRequest.Add(*container.Resources.Requests.Memory())
+			}
+			nodeResources[pod.Spec.NodeName] = nodeResource
+		}
+
+		for key, node := range nodeResources {
+			fmt.Printf("%s: Memory: %dMi request (%d%%), %dMi limit (%d%%), %dMi total available. Pods: %d/%d pods (%d%%)\n",
+				key,
+				node.memoryRequest.ScaledValue(resource.Mega),
+				node.memoryRequest.ScaledValue(resource.Mega)*100 / node.maxMemoryAvailable.ScaledValue(resource.Mega),
+				node.memoryLimit.ScaledValue(resource.Mega),
+				node.memoryLimit.ScaledValue(resource.Mega)*100 / node.maxMemoryAvailable.ScaledValue(resource.Mega),
+				node.maxMemoryAvailable.ScaledValue(resource.Mega),
+				node.pods,
+				node.maxPodsAvailable,
+				node.pods*100/node.maxPodsAvailable,
+			)
+		}
+
 		os.Exit(0)
 	}
 
